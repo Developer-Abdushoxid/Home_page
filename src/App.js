e40f3438dcf1fad0dd66e0d839c1223865c00b0d
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-07-09 15:30:04 +0500

    added function

--- src/App.js
+++ src/App.js
@@ -10,23 +10,54 @@ import { useState, useEffect } from "react";
 
 // export default App
 
-function Counter (){
-  const [count, setCount] = useState(0)
-  const [calculate, setCalculate] = useState(0)
+// function Counter(){
+//   const [count, setCount] = useState(0)
+//   const [calculate, setCalculate] = useState(0)
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setCalculate( count * 2)
+//     })
+//     });
+   
+
+//   return (
+//     <>
+//     <p>Count: {count}</p>
+//     <p>Calculate: {calculate}</p>
+//     <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </>
+//   )
+// }
+
+// export default Counter;
+
+
+// const Counter = () => {
+// const [count, setCount] = useState(0)
+//  function handClick () {
+//   setCount(count + 1)
+//  }
+
+//  return (
+//   <button onClick={handClick}>clicked {count} times</button>
+//  )
+// };
+
+// export default Counter;
+
+import ReactDOM from "react-dom/client";
+
+function Counter() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCalculate(() => count * 3)
-  },[count])
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
 
-  return(
-    <>
-    <p>Hisoblash: {count}</p>
-    <button onClick={() => setCount((c) => c + 1)}> + </button>
-    <p>Calculate: {calculate}</p>
-    </>
-  )
+  return <h1>I've rendered {count} times!</h1>;
 }
-
 export default Counter;
 
 
