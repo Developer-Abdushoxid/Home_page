47cec72e2995f7857f2dbbec185a600c61fe3a5d
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-07-09 12:02:27 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 // import Navbar from "./navbar/navbar"
 
 // const App = () => {
@@ -7,3 +9,24 @@
 // }
 
 // export default App
+
+function Counter (){
+  const [count, setCount] = useState(0)
+  const [calculate, setCalculate] = useState(0)
+
+  useEffect(() => {
+    setCalculate(() => count * 3)
+  },[count])
+
+  return(
+    <>
+    <p>Hisoblash: {count}</p>
+    <button onClick={() => setCount((c) => c + 1)}> + </button>
+    <p>Calculate: {calculate}</p>
+    </>
+  )
+}
+
+export default Counter;
+
+
